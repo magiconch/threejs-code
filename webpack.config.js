85f7e96501e03316acb9850d0bbdb2714e1415e0
magiconch <magiconch@outlook.com>
2022-04-25 22:19:44 +0800

    Add configs

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/index.ts",
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './src',
+    },
+    resolve: {
+        // Add '.ts' and '.tsx' as a resolvable extension.
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+            {
+                test: /\.ts?$/,
+                exclude: /node_modules/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
